4626aeea1502e2b4571dda0c278f068295fc1585
NurbekSaitqulov03 <nurbek.saitqulov23@gmail.com>
2023-02-09 14:50:38 +0500

    file04.py

--- file04.py
+++ file04.py
@@ -6,5 +6,12 @@ def main(data:str):
     Returns:
         list: return answer
     """
-    
+    f = open(data, 'r')
+    a = f.read()
+    x = []
+    for i in a:
+        if not i.isdigit():
+            x.append(i)
+    return x
 # Read data from file
+print(main('txt_file/data04.txt'))
